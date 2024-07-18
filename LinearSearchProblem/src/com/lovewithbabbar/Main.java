9b1a9a5773cacb723c92904ff6495b1d96ccf9c4
tush2809 <sabanetushar@gmail.com>
2024-07-18 18:35:20 +0530

    added all changes ....

--- LinearSearchProblem/src/com/lovewithbabbar/Main.java
+++ LinearSearchProblem/src/com/lovewithbabbar/Main.java
@@ -6,5 +6,5 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
-    }
+  }
 }
--- SortingTechnique/src/com/lovewithbabbar/SelectionSort.java
+++ SortingTechnique/src/com/lovewithbabbar/SelectionSort.java
@@ -1,7 +1,18 @@
 package com.lovewithbabbar;
 
-public class SelectionSort {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] inputArray={6,8,9,7,56};
+        System.out.println(Arrays.toString(selectionSort(inputArray)));
+    }
+
+    static int[] selectionSort(int[] inputArray){
+
+
+
+        return inputArray;
     }
 }
